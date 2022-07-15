425a7637d5468eb3e029c50c89fff0bc4edd69fc
unknown <srijandas18.sd@gmail.com>
2022-07-15 12:04:49 +0530

    minor styling change

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -8,47 +8,48 @@ function Header({ isAuthenticated, cart }) {
 
   return (
     <>
-      <nav className="flex flex-wrap sticky top-0 z-40 p-3 bg-gray-800 justify-between items-center">
-        <div className="navBrand flex items-center text-lg font-semibold text-white cursor-pointer">
-          <HiMenu onClick={() => setShowSidebar(true)} />
-          <Link href="/">
-            <h2 className="ml-2">Next Footwear</h2>
-          </Link>
-        </div>
-        <div className="buttons flex gap-2 md:order-last">
-          {isAuthenticated ? (
-            <Link href={"/account"}>
-              <button className="btn-white">My Account</button>
+      <nav className="bg-gray-800 sticky top-0 z-40">
+        <div className="flex flex-wrap mx-auto p-3 justify-between items-center max-w-[2000px]">
+          <div className="navBrand flex items-center text-lg font-semibold text-white cursor-pointer">
+            <HiMenu onClick={() => setShowSidebar(true)} />
+            <Link href="/">
+              <h2 className="ml-2">NFootwear</h2>
             </Link>
-          ) : (
-            <Link href={"/login"}>
-              <button className="btn-white">Login</button>
-            </Link>
-          )}
+          </div>
+          <div className="buttons flex gap-2 md:order-last">
+            {isAuthenticated ? (
+              <Link href={"/account"}>
+                <button className="btn-white">My Account</button>
+              </Link>
+            ) : (
+              <Link href={"/login"}>
+                <button className="btn-white">Login</button>
+              </Link>
+            )}
 
-          <Link href="/cart">
-            <button className="btn-green">
-              <HiShoppingCart className="mr-2" />
-              Cart ({cart.totalItems})
-            </button>
-          </Link>
-        </div>
-        <div className="search mt-3 w-full md:mt-0 md:w-[60%]">
-          <form className="flex bg-gray-50 rounded-lg">
-            <input
-              type="search"
-              id="default-search"
-              className="block p-2.5 w-full text-sm outline-none text-gray-900 rounded-lg"
-              placeholder="Search here..."
-              required
-            />
-            <button type="submit" className="btn-green">
-              <HiSearch />
-            </button>
-          </form>
+            <Link href="/cart">
+              <button className="btn-green">
+                <HiShoppingCart className="mr-2" />
+                Cart ({cart.totalItems})
+              </button>
+            </Link>
+          </div>
+          <div className="search mt-3 w-full md:mt-0 md:w-[60%]">
+            <form className="flex bg-gray-50 rounded-lg">
+              <input
+                type="search"
+                id="default-search"
+                className="block p-2.5 w-full text-sm outline-none text-gray-900 rounded-lg"
+                placeholder="Search here..."
+                required
+              />
+              <button type="submit" className="btn-green">
+                <HiSearch />
+              </button>
+            </form>
+          </div>
         </div>
       </nav>
-
       <Sidebar setShowSidebar={setShowSidebar} showSidebar={showSidebar} />
     </>
   );
