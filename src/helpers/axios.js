47605384699dcfdf2a6f0b9070abed8a0b9dc3a6
unknown <srijandas18.sd@gmail.com>
2022-07-19 01:16:57 +0530

    deploy

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-// const environment = env.NODE_ENV || "development";
-const environment = "development";
+const environment = env.NODE_ENV || "development";
 
 const instance = axios.create({
   baseURL:
     environment === "development"
       ? process.env.NEXT_PUBLIC_DEV_API_URL
-      : process.env.API_URL,
+      : process.env.NEXT_PUBLIC_API_URL,
 });
 
 export default instance;
