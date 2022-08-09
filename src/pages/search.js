736a0d4d94f13ca1feb53a02a486edd80ac8e04a
unknown <srijandas18.sd@gmail.com>
2022-08-09 23:17:18 +0530

    fixed module not found on search page

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import ProductCard from "../components/shared/ProductCard";
 import axios from "../utils/axios";
 import { useDispatch } from "react-redux";
 import { useRouter } from "next/router";
-import Filter from "../components/filters/Filter";
+import { HiFilter, HiSortAscending } from "react-icons/hi";
+import SorterModal from "../components/filters/SorterModal";
+import FilterModal from "../components/filters/FilterModal";
 
-function search({ products }) {
+function search(pageProps) {
   const dispatch = useDispatch();
   const router = useRouter();
+  const [products, setProducts] = useState(pageProps.products);
+
+  // *****
+  // sorting
+  // *****
+  const [sortModalOpen, setSortModalOpen] = useState(false);
+  const toggleSortModal = () => setSortModalOpen(!sortModalOpen);
+  const [sortMethod, setSortMethod] = useState("popularity");
+
+  const handleSortMethodChange = (method) => {
+    setSortMethod(method);
+    if (method === "low-to-high") {
+      setProducts(products.sort((a, b) => a.starting_price - b.starting_price));
+    } else if (method === "high-to-low") {
+      setProducts(products.sort((a, b) => b.starting_price - a.starting_price));
+    } else {
+      setProducts(products.sort((a, b) => b.created_at - a.created_at));
+    }
+
+    toggleSortModal();
+  };
+
+  // *****
+  // filtering
+  // *****
+
+  const [filterModalOpen, setFilterModalOpen] = useState(false);
+  const toggleFilterModal = () => setFilterModalOpen(!filterModalOpen);
 
   useEffect(() => {
     dispatch({
@@ -18,8 +48,29 @@ function search({ products }) {
 
   return (
     <div className="md:p-20">
-      <Filter />
-
+      <div className="flex items-center w-full md:justify-end btn-group mb-2">
+        <button
+          onClick={toggleSortModal}
+          className="btn btn-ghost gap-2 w-1/2 md:w-auto rounded-none md:rounded-md border-r-2 border-r-slate-200 shadow-md"
+        >
+          <HiSortAscending className="h-5 w-5" />
+          Sort
+        </button>
+        <button
+          onClick={toggleFilterModal}
+          className="btn btn-ghost gap-2 w-1/2 md:w-auto rounded-none md:rounded-md border-l-2 border-l-slate-200 shadow-md"
+        >
+          <HiFilter className="h-5 w-5" />
+          Filter
+        </button>
+        <SorterModal
+          isOpen={sortModalOpen}
+          onClose={toggleSortModal}
+          sortMethod={sortMethod}
+          handleSortMethodChange={handleSortMethodChange}
+        />
+        <FilterModal isOpen={filterModalOpen} onClose={toggleFilterModal} />
+      </div>
       <div className="h-screen md:mt-5">
         {products.length ? (
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
